96d83d43656ce3662f17caf3f8d718c6972918f3
Tyler Ray <tyler.p.ray@icloud.com>
2021-04-01 21:48:22 -0400

    first commit

---
+++ src/components/GuessPeg/GuessPeg.js
@@ -0,0 +1,10 @@
+import styles from './GuessPeg.module.css';
+
+const GuessPeg = (props) => (
+  <div
+    className={styles.peg}
+    style={{backgroundColor: props.color}}
+  />
+);
+
+export default GuessPeg;
